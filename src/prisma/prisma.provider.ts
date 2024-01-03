fbc7812472291a19cfe845ccd9e81bb4fcc4a6c1
KrittawatMC <tiees288@gmail.com>
2024-01-03 20:18:34 +0700

    Rebuild prisma

--- src/prisma/prisma.provider.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { Provider } from '@nestjs/common';
-
-export const PrismaProvider: Provider = {
-  provide: PrismaClient,
-  useValue: new PrismaClient(),
-};
